a6f000e610ae090abb546c37323c96eb4c3f7a3d
Brad Gravesen <bgravesen@hotmail.com>
2023-12-02 14:49:13 -0800

    first commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest, outputDocument: func.Out[func.Document],
+    ) -> str:
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        outputDocument.set(func.Document.from_dict({"id": name}))
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
